a8f365aad83e5337889ae3b5d19693034a870d86
Sofia Krylova <sofia_krylova@epam.com>
2022-12-26 12:53:35 +0100

    Initial

---
+++ src/main/java/com/epam/autotasks/CatDatabase.java
@@ -0,0 +1,18 @@
+package com.epam.autotasks;
+
+import java.util.List;
+
+public class CatDatabase {
+
+    public List<String> getCatNamesByBreed(List<Cat> cats, Cat.Breed breed) {
+        return null;
+    }
+
+    public List<Cat> filterYoungerCatsByAge(List<Cat> cats, Integer age) {
+        return null;
+    }
+
+    public List<Cat> filterCatsByNamePrefix(List<Cat> cats, String prefix) {
+        return null;
+    }
+}
